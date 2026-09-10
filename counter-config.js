c5651e5a171ee37a857ffa90bf50fcb15004433e
gamyaragon <gamyaragon@gmail.com>
2026-09-10 16:41:15 +0800

    Add files via upload

--- counter-config.js
+++ counter-config.js
@@ -1,4 +1,4 @@
 window.GGYA_ANALYTICS = {
-  supabaseUrl: "https://ginkpnkblyuuzwdknvz.supabase.co",
+  supabaseUrl: "https://gvirkprkhlyuuzwdkrvz.supabase.co",
   supabaseAnonKey: "sb_publishable_fgB8Pg0VQkYdLINbIBWb4w__TrRUevu"
 };
